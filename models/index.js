d5ffd9c7c1fd277138749d20b24c5667d020bb19
Hersh Amin <h.amin@utexas.edu>
2015-10-28 19:13:15 -0500

    Added model relationships and export statements

--- models/index.js
+++ models/index.js
@@ -14,10 +14,22 @@ sequelize.authenticate().then(function (result) {
 });
 
 // Load models
-//
+var models = [
+	'Question',
+	'Choice',
+	'Vote'
+]
+models.forEach(function (model) {
+	module.exports[model] = sequelize.import(__dirname + '/' + model);
+});
 
 // Describe relationships
-//
+(function (m) { // Question can have many Choices, Each Choice can have many Votes
+	m.Choice.belongsTo(m.Question);
+	m.Question.hasMany(m.Choice); // Each question can have many choices
+	m.Vote.belongsTo(m.Choice);
+	m.Choice.hasMany(m.Vote); // Each choice can have many votes
+})(module.exports);
 
 // Export connection
 module.exports.sequelize = sequelize
