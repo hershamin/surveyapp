f1e879c23a69ead894bedb7690116468ab7d93f5
Hersh Amin <h.amin@utexas.edu>
2015-10-31 22:11:23 -0500

    Revert authentication

--- server.js
+++ server.js
@@ -3,37 +3,6 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('config.json','utf8'));
-var session = require('express-session');
-var flash = require('connect-flash');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var dbOperations = require('./dbOps.js');
-
-// Setup authentication strategy
-passport.serializeUser(function (user, done) {
-	done(null, user);
-});
-passport.deserializeUser(function (user, done) {
-	done(null, user);
-});
-passport.use(new LocalStrategy({
-					usernameField: 'username',
-					passwordField: 'password'},
-
-					function (username, password, done) {
-						if (username != config.adminUserName || password != config.adminPassword) {
-							return done(null, false, {message:'Incorrect Username and/or Password'})
-						} else {
-							return done(null, {username:config.adminUserName, password:config.adminPassword});
-						}
-					})
-);
-app.use(passport.initialize());
-app.use(passport.session());
-function ensureAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) { return next(); }
-	res.redirect('/login');
-}
 
 // Set DB
 app.set('models', require('./models'))
@@ -53,27 +22,7 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 
-app.get('/login', function (req, res, next) {
-	res.render('login');
-});
-
-app.post('/login', function (req, res, next) {
-	passport.authenticate('local', function (err, user, info) {
-		if (err) { return next(err) }
-		if (!user) { return res.redirect('/login') }
-		req.logIn(user, function (err) {
-			if (err) { return next(err) }
-			return res.redirect(req.redirect('/admin'));
-		})
-	})(req, res, next)
-});
-
-app.get('/logout', function (req, res) {
-	req.logout();
-	res.redirect('/login');
-})
-
-app.get('/admin', ensureAuthenticated, function (req, res, next) {
+app.get('/admin', function (req, res) {
 	res.render('admin');
 });
 
@@ -81,7 +30,7 @@ app.get('/polls', function (req, res) {
 	res.render('polls');
 });
 
-app.post('/question', ensureAuthenticated, function (req, res, next) {
+app.post('/question', function (req, res) {
 	if (!req.body) return res.sendStatus(400);
 	dbOperations.postQuestion(req, res, app);
 });
